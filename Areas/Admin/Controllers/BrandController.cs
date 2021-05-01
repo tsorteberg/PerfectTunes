eda0ee026d09015f272bb7dd63feb1cd3ef4a07e
Tom Sorteberg <66091315+tsorteberg@users.noreply.github.com>
2021-05-01 01:19:04 -0500

    Added managing brands in Admin Console.

--- Areas/Admin/Controllers/BrandController.cs
+++ Areas/Admin/Controllers/BrandController.cs
@@ -17,6 +17,7 @@ using Microsoft.AspNetCore.Mvc;
 using PerfectTunes.Models;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -24,7 +25,7 @@ namespace PerfectTunes.Areas.Admin.Controllers
 {
     [Area("Admin")]
     public class BrandController : Controller
-    {   
+    {
         private PerfectTunesUnitOfWork data { get; set; }
         private readonly IWebHostEnvironment hostingEnvironment;
         public BrandController(PerfectTunesContext ctx, IWebHostEnvironment hostingEnvironment)
@@ -58,6 +59,113 @@ namespace PerfectTunes.Areas.Admin.Controllers
             };
 
             return View(vm);
+        }
+
+        [HttpGet]
+        public ViewResult Add(int id) => GetBrand(id, "Add");
+
+        [HttpPost]
+        public IActionResult Add(BrandViewModel vm)
+        {
+            if (ModelState.IsValid)
+            {
+                string uniqueFileName = null;
+
+                if (vm.Image != null)
+                {
+                    string uploadsFolder = Path.Combine(hostingEnvironment.WebRootPath, "images");
+                    uniqueFileName = Guid.NewGuid().ToString() + "_" + vm.Image.FileName;
+                    string filePath = Path.Combine(uploadsFolder, uniqueFileName);
+                    vm.Image.CopyTo(new FileStream(filePath, FileMode.Create));
+                    vm.Brand.LogoImage = uniqueFileName;
+                }
+                else
+                {
+                    vm.Brand.LogoImage = "comingsoon.jpg";
+                }
+
+                data.Brands.Insert(vm.Brand);
+                data.Save();
+
+                TempData["message"] = $"{vm.Brand.BrandName} added to Instruments.";
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                Load(vm, "Add");
+                return View("Brand", vm);
+            }
+        }
+
+        [HttpGet]
+        public ViewResult Edit(int id) => GetBrand(id, "Edit");
+
+        [HttpPost]
+        public IActionResult Edit(BrandViewModel vm)
+        {
+            if (ModelState.IsValid)
+            {
+                string uniqueFileName = null;
+
+                if (vm.Image != null)
+                {
+                    string uploadsFolder = Path.Combine(hostingEnvironment.WebRootPath, "images");
+                    uniqueFileName = Guid.NewGuid().ToString() + "_" + vm.Image.FileName;
+                    string filePath = Path.Combine(uploadsFolder, uniqueFileName);
+                    vm.Image.CopyTo(new FileStream(filePath, FileMode.Create));
+                    vm.Brand.LogoImage = uniqueFileName;
+                }
+                else
+                {
+                    vm.Brand.LogoImage = "comingsoon.jpg";
+                }
+
+                data.Brands.Update(vm.Brand);
+                data.Save();
+
+                TempData["message"] = $"{vm.Brand.BrandName} updated.";
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                Load(vm, "Edit");
+                return View("Instrument", vm);
+            }
+        }
+
+        [HttpGet]
+        public ViewResult Delete(int id) => GetBrand(id, "Delete");
+
+        [HttpPost]
+        public IActionResult Delete(BrandViewModel vm)
+        {
+            data.Brands.Delete(vm.Brand);
+            data.Save();
+            TempData["message"] = $"{vm.Brand.BrandName} removed from Brands.";
+            return RedirectToAction("Index");
+        }
+
+        private ViewResult GetBrand(int id, string operation)
+        {
+            var brand = new BrandViewModel();
+            Load(brand, operation, id);
+            return View("Brand", brand);
+        }
+
+        private void Load(BrandViewModel vm, string op, int? id = null)
+        {
+            if (Operation.IsAdd(op))
+            {
+                vm.Brand = new Brand();
+            }
+            else
+            {
+                vm.Brand = data.Brands.Get(new QueryOptions<Brand>
+                {
+                    //Includes = "Brand",
+                    Where = b => b.BrandId == (id ?? vm.Brand.BrandId)
+                });
+            }
         }
     }
 }
